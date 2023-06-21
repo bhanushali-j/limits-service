8c40a7596b19de58a722a0ac099fce148c4b72fb
bhanushalij <bhanushalij@PMCLAP1607-1222.pmc.local>
2023-06-21 23:39:44 +0530

    First commit with limit-service where it is taking properties maximium and minimum from the application.properties file

---
+++ src/main/java/com/udemy/in28minutes/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,22 @@
+package com.udemy.in28minutes.microservices.limitsservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.udemy.in28minutes.microservices.limitsservice.bean.Limits;
+import com.udemy.in28minutes.microservices.limitsservice.configuration.Configuration;
+
+@RestController
+public class LimitsController {
+	
+	@Autowired
+	Configuration configuration;
+	
+	
+	@GetMapping("/limits")
+	public Limits retrieveLimits() {
+		return new Limits(configuration.getMinimum(),configuration.getMaximum());
+	}
+}
